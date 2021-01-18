f9007ff71a7def78fc07e6e527798984302409c4
Mustafashahoud <mustafa.shahoud@gmail.com>
2021-01-18 01:37:42 +0100

    Refactored.

--- app/src/main/java/com/android/mustafa/applicationA/feature/model/NotificationEntity.kt
+++ app/src/main/java/com/android/mustafa/applicationA/feature/model/NotificationEntity.kt
@@ -26,26 +26,26 @@ data class NotificationEntity(
 
     @ColumnInfo(name = COLUMN_TIME)
     var time: Long = 0L
-)  {
+) {
     companion object {
         const val TABLE_NAME = "notifications"
 
         /** The name of the ID column.  */
         const val COLUMN_ID = BaseColumns._ID
 
-        /** The name of the name column.  */
+        /** The name of the title column.  */
         const val COLUMN_TITLE = "title"
 
-        /** The name of the name column.  */
+        /** The name of the packageName column.  */
         const val COLUMN_PACKAGE_NAME = "packageName"
 
-        /** The name of the name column.  */
+        /** The name of the message column.  */
         const val COLUMN_MESSAGE = "message"
 
-        /** The name of the name column.  */
+        /** The name of the importance column.  */
         const val COLUMN_IMPORTANCE = "importance"
 
-        /** The name of the name column.  */
+        /** The name of the time column.  */
         const val COLUMN_TIME = "time"
 
         fun fromContentValues(values: ContentValues?): NotificationEntity {
